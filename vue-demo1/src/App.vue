233e841231b4142f6cdec22875f5951bb3ccd18b
hub6622 <1633321257xiao@gmail.com>
2024-05-16 12:48:47 +0800

    first commit

---
+++ vue-demo1/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data(){
+        return{
+            msg:"ppg666",
+            hello:"hello world",
+            html:"<a href='https://www.baidu.com'>百度</a>"
+        }
+    }
+}
+</script>
+
+<template>
+  <h3>文本插值</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
+  <h3>html传输</h3>
+  <p v-html="html"></p>
+</template>
---
+++ vue-demo2/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data(){
+    return{
+      msg:"ppg666",
+      dynamicclass:"appClass",
+      dynamicid:"appId",
+      buttonCondition:false,
+      Attrs:{
+        class:"appClass",
+        id:"appId"
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button :disabled="buttonCondition">Button</button><br>
+  <a href="#" v-bind:class="dynamicclass" :id="dynamicid">000</a>
+  <div v-bind="Attrs">666</div>
+</template>
+
+<style>
+.appClass{
+  color: red;
+  font-size: 30px;
+}
+</style>
