b83dbc82acd5d0fdc76cd0ae2ad5d494ad1abc92
hub6622 <1633321257xiao@gmail.com>
2024-05-16 13:02:36 +0800

    add DemoVue

--- vue-demo2/src/App.vue
+++ vue-demo2/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import DemoVue from './components/DemoVue.vue'
 </script>
 
 <template>
   
   <HelloWorld/>
-    
+  <DemoVue/>
 </template>
---
+++ vue-demo2/src/components/DemoVue.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>条件渲染</h3>
+    <div v-if="type === 'A'">could you see me???</div>
+    <div v-else-if="type === 'B'">看我</div>
+    <div v-else>666</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            flag:false,
+            type:"A"
+        }
+    }
+}
+</script>
